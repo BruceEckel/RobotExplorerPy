b25a91af4d12f176868ee3e3cfaebb8f1cd30b1c
Bruce Eckel <mindviewinc@gmail.com>
2019-12-26 00:04:15 -0800

    Update clear_screen.py

--- clear_screen.py
+++ clear_screen.py
@@ -1,3 +1,9 @@
+"""
+Clears the terminal screen regardless of OS
+"""
+import platform
 import os
-def clear_screen(): os.system('cls')
 
+
+def clear_screen():
+    os.system("cls" if platform.system().lower() == "windows" else "clear")
